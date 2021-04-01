96a4ca874ce7c23b9e49ad8c2209f0b4ae83b50c
pedromaaiaa <pedroceduca@gmail.com>
2021-04-01 11:22:06 -0300

    ready to go

--- app/app.py
+++ app/app.py
@@ -20,35 +20,30 @@ def home():
     keyword.clear()
     data.clear()
     submission.clear()
-    if request.method == 'GET':
-        print('ge')
-    else:
-        if form.validate_on_submit():
-            keyword.clear()
-            data.clear()
-            submission.clear()
-            if "www.youtube.com/watch?v=" in form.youtube_link.data: 
-                link = form.youtube_link.data
-                key = form.keyword.data
-                keyword.append(key)
+    if form.validate_on_submit():
+        keyword.clear()
+        data.clear()
+        submission.clear()
+        if "www.youtube.com/watch?v=" in form.youtube_link.data: 
+            link = form.youtube_link.data
+            key = form.keyword.data
+            keyword.append(key)
 
-                url_parsed = parse.urlparse(link)
-                qsl = parse.parse_qs(url_parsed.query)
-                transcript_list = YouTubeTranscriptApi.list_transcripts(qsl["v"][0])
+            url_parsed = parse.urlparse(link)
+            qsl = parse.parse_qs(url_parsed.query)
+            transcript_list = YouTubeTranscriptApi.list_transcripts(qsl["v"][0])
 
-                for x in transcript_list:
-                    text = x.fetch()
+            for x in transcript_list:
+                text = x.fetch()
 
-                for x in text:
-                    x['link'] = f"{link}={int(x['start'])}s"
-                    data.append(x)
-                    if key in x['text']:
-                        submission.append(1)
-                if not submission:
-                    flash('Nothing was found!')
+            for x in text:
+                x['link'] = f"{link}&t={int(x['start'])}s"
+                data.append(x)
+                if key in x['text']:
+                    submission.append(1)
 
-            else: 
-                flash('Error trying to get the link!')
+        else: 
+            flash('Error trying to get the link!')
 
     return render_template(
                 'home.html',
@@ -59,4 +54,3 @@ def home():
                 data=data,
                 submission=submission
             )
-    
